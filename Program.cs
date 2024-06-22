0aaf86f851e748f0b74e3cd6e23e17105cd2baae
phanvantien99 <neetkite@gmail.com>
2024-06-22 21:32:35 +0700

    Initialize report practice C#

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿namespace CoderSchool_L1;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        //Part 1 Print a string to the console
+        //hint: Console.WriteLine("Hello World!");
+        Console.WriteLine("Hello, World!");
+
+        //Part 2 Get user input and print it to the console
+        //hint: Console.ReadLine();
+        string? s1 = Console.ReadLine();
+        string? s2 = Console.ReadLine();
+        
+        //Part 3 Get two inputs from the user and print them on separate lines
+        //hint: use \n to create a new line
+        Console.WriteLine("Input 1: " + s1 + "\nInput 2: " + s2);
+    }
+}
